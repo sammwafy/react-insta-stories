21792b0ecf5462a0535779720a9c81a72f72725d
sammwafy <59634544+sammwafy@users.noreply.github.com>
2022-04-15 16:23:36 +0200

    Update withSeeMore.tsx

--- src/renderers/wrappers/withSeeMore.tsx
+++ src/renderers/wrappers/withSeeMore.tsx
@@ -5,7 +5,7 @@ import SeeMore from "./../../components/SeeMore";
 const withSeeMore: React.FC<{
     story: Story,
     action: Action,
-    ref: ref,
+    ref: SeeMoreProps["ref"],
     customCollapsed?: SeeMoreProps["customCollapsed"]
 }> = ({ story, action, customCollapsed, children,ref }) => {
     const [showMore, setShowMore] = useState(false);
